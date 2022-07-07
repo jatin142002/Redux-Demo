23067556c2d13a5ee2379da29cade3877b09e49a
JatinMehra <mehrajatin142002@gmail.com>
2022-07-07 15:49:47 +0530

    Multiple Reducer v1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux');
 const createStore = redux.legacy_createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 
 //-----> Action is a javascript object , which must have type as key and it has string value <-----------
@@ -20,12 +21,20 @@ function buycake(){
     }
 }
 
+function buyicecream(){
+    return {
+        type: BUY_ICECREAM,
+        info: "Second redux action"
+    }
+}
+
 //--> Reducers are the function which accepts state and action as an argument and returns the next state of the application 
 //--->   (previousState , action) => newState
 
 //----------> Initial State <--------------
 const initialState = {
-    numofCakes: 10
+    numofCakes: 10,
+    numofIceCreams: 10
 }   
 
 const reducer = (state = initialState, action)=>{
@@ -33,7 +42,13 @@ const reducer = (state = initialState, action)=>{
     switch(action.type)
     {
         case BUY_CAKE: return{
-            numofCakes: state.numofCakes - 1
+            numofCakes: state.numofCakes - 1,
+            numofIceCreams: state.numofIceCreams
+        }
+
+        case BUY_ICECREAM: return{
+            numofCakes: state.numofCakes - 1,
+            numofIceCreams: state.numofIceCreams - 1
         }
 
         default: return state
@@ -52,4 +67,6 @@ const unsubscribe = store.subscribe(()=>console.log("Updated state", store.getSt
 store.dispatch(buycake());
 store.dispatch(buycake());
 store.dispatch(buycake());
+store.dispatch(buyicecream());
+store.dispatch(buyicecream());
 unsubscribe();
