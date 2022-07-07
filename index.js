b77f5ebe95a6ba98765f111a0507aec8a175353c
JatinMehra <mehrajatin142002@gmail.com>
2022-07-07 13:46:43 +0530

    Redux Store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');  
+const createStore = redux.legacy_createStore;
+
 const BUY_CAKE = 'BUY_CAKE';
 
 
@@ -36,3 +39,17 @@ const reducer = (state = initialState, action)=>{
         default: return state
     }
 }
+
+// Redux Store ---> One store for the entire application
+// i) Holds application state
+//ii) Allows access to state via getState()
+//iii)Allows state to be updated via dispatch(action)
+//iv)Registers listeners via subscribe(listener) 
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(()=>console.log("Updated state", store.getState()));
+store.dispatch(buycake());
+store.dispatch(buycake());
+store.dispatch(buycake());
+unsubscribe();
